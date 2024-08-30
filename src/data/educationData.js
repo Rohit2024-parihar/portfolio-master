f1a5f8ae7a52276254d6a0c0ed1313f8ccade8b1
Rohit Parihar <63337003+Rohit2024-parihar@users.noreply.github.com>
2024-08-30 18:08:28 +0530

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -3,7 +3,7 @@ export const educationData = [
         id: 1,
         institution: 'Galgotias University',
         course: 'B.Tech',
-        cgpa:'8.8 cgpa',
+        cgpa:'8.8 CGPA',
         startYear: '2018',
         endYear: '2022'
     },
@@ -11,7 +11,7 @@ export const educationData = [
         id: 2,
         institution: 'Agra Public School',
         course: 'intermediate',
-        cgpa:'7.2 cgpa',
+        cgpa:'8.0 CGPA',
         startYear: '2017',
         endYear: '2018'
     },
@@ -19,7 +19,7 @@ export const educationData = [
         id: 3,
         institution: 'Agra Public School',
         course: 'High School',
-        cgpa:'8.2 cgpa',
+        cgpa:'8.2 CGPA',
         startYear: '2015',
         endYear: '2016'
     },
