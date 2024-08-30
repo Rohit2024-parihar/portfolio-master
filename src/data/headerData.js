9485947b4a1e0cf0601cb224a34749ffe0406d50
Rohit Parihar <63337003+Rohit2024-parihar@users.noreply.github.com>
2024-08-30 18:01:27 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Rohit Parihar',
     title: "Web Developer",
     desciption:"Coding is the one thing that I started in my college life to increase my problem solving skills and get a good job and I achieved that. Little did I know that I will eventually fell for it.",
-    image: 'https://media.licdn.com/dms/image/C4D03AQHHOxEfKrlorw/profile-displayphoto-shrink_200_200/0/1652929978545?e=1721865600&v=beta&t=-XFQdgiYcQ94VW-d347ZRtshd_F9CHalISY0SX6Yk8s',
+    image: 'https://media.licdn.com/dms/image/v2/C4D03AQHHOxEfKrlorw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1652929978545?e=1730332800&v=beta&t=-9x4TkKmb0L3j4MjE4hpq51s_v9zhScUGCmKxwTZ3J8',
     resumePdf: Rohit_Parihar_Resume
 }
