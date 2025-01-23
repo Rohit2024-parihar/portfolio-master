4434f3649e423789dbb458bf6881ad8ebba2c4ca
Rohit Parihar <63337003+Rohit2024-parihar@users.noreply.github.com>
2025-01-23 19:03:18 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,8 +2,8 @@ import Rohit_Parihar_Resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Rohit Parihar',
-    title: "Web Developer",
+    title: "Frontend Developer",
     desciption:"Coding is the one thing that I started in my college life to increase my problem solving skills and get a good job and I achieved that. Little did I know that I will eventually fell for it.",
-    image: 'https://media.licdn.com/dms/image/v2/C4D03AQHHOxEfKrlorw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1652929978545?e=1730332800&v=beta&t=-9x4TkKmb0L3j4MjE4hpq51s_v9zhScUGCmKxwTZ3J8',
+    image: 'https://media.licdn.com/dms/image/v2/C4D03AQHHOxEfKrlorw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1652929978545?e=1743033600&v=beta&t=rvmARe9cH8SpaBOBp8En5MY5hMAYlf5wSkoTZc9ijbo',
     resumePdf: Rohit_Parihar_Resume
 }
